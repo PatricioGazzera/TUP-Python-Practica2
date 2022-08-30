517919b40a092c6d75ef8cf1bb8a815c723d47da
Antonio Torralba <antoniotorralba0403@gmail.com>
2022-08-30 12:11:36 -0300

    ej1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -55,9 +55,7 @@ def maximo_ternario(a: float, b: float) -> float:
 
 
 # NO MODIFICAR - INICIO
-    if (a>b):
-        return a
-    return b
+    maximo_ternario = "si" if a >= b else "no"
 
 assert maximo_ternario(10, 5) == 10
 assert maximo_ternario(9, 18) == 18
