f8e32c92ad0625b9b299249a137cb80c2aebbbb0
Antonio Torralba <antoniotorralba0403@gmail.com>
2022-09-14 11:09:47 -0300

    ej7

--- exercises/exercise7.py
+++ exercises/exercise7.py
@@ -1,5 +1,7 @@
 """Any y Sets."""
 
+from operator import truediv
+from pickle import FALSE, TRUE
 from typing import Any, Iterable
 
 
@@ -14,6 +16,13 @@ def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool
 
 
 # NO MODIFICAR - INICIO
+    i:0
+    x:0
+    for i in lista_1:
+        for x in lista_2:
+            if i == x:
+                return TRUE
+    return FALSE
 test_list = [1, "hello", 35.20]
 assert superposicion_basico(test_list, (2, "world", 35.20))
 assert not superposicion_basico(test_list, (2, "world", 30.85))
@@ -33,6 +42,12 @@ def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
 
 
 # NO MODIFICAR - INICIO
+    for lista_1 in lista_2:
+        if lista_1 in lista_2:
+            return True
+    return False
+
+
 test_list = [1, "hello", 35.20]
 assert superposicion_in(test_list, (2, "world", 35.20))
 assert not superposicion_in(test_list, (2, "world", 30.85))
@@ -55,6 +70,8 @@ def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
 
 
 # NO MODIFICAR - INICIO
+    print(Any(superposicion_any))
+
 test_list = [1, "hello", 35.20]
 assert superposicion_any(test_list, (2, "world", 35.20))
 assert not superposicion_any(test_list, (2, "world", 30.85))
@@ -76,6 +93,9 @@ def superposicion_set(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
 
 
 # NO MODIFICAR - INICIO
+print(set(superposicion_set))
+
+
 test_list = [1, "hello", 35.20]
 assert superposicion_set(test_list, (2, "world", 35.20))
 assert not superposicion_set(test_list, (2, "world", 30.85))
